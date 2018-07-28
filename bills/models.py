c11d904c55526a3254c64d67323bcc7781f02601
Elysson MR <elyssonmr@gmail.com>
2018-07-28 19:07:42 -0300

    Add the start/end records filter

--- bills/models.py
+++ bills/models.py
@@ -1,17 +1,53 @@
-from django.db import models
+from datetime import datetime, timedelta
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+def cycle_date(billing_cycle):
+    if not billing_cycle:
+        now = timezone.now()
+        billing_cycle = f"{now.month:02}/{now.year}"
+    month, year = map(int, billing_cycle.split("/"))
+    start_date = timezone.make_aware(
+        datetime(day=1, month=month, year=year))
+    end_date = timezone.make_aware(
+        datetime(day=1, month=month + 1, year=year) - timedelta(days=1))
+    
+    return start_date, end_date
+
+
+class CallRecordManager(models.Manager):
+    def get_bill_records(self, subscriber, billing_cycle=None):
+        call_records = self.filter(
+            source=subscriber, timestamp__range=cycle_date(billing_cycle)
+        )
+        start_calls = {}
+        end_calls = {}
+        for record in call_records:
+            if record.record_type == "S":
+                start_calls[record.call_id] = record
+            else:
+                end_calls[record.call_id] = record
+        records = []
+        for end_call_id in end_calls:
+            start_record = start_calls.get(end_call_id, None)
+            if not start_record:
+                continue
+            records.append((start_record, end_calls[end_call_id]))
+        return records
 
 
 class CallRecord(models.Model):
+    objects = CallRecordManager()
     STARTING_CALL = "S"
     END_CALL = "E"
     CALL_TYPE = ((STARTING_CALL, "Starting Call"), (END_CALL, "End Call"))
     record_type = models.CharField(
         "Record Type", max_length=1, choices=CALL_TYPE, default=STARTING_CALL
     )
-    timestamp = models.DateTimeField("Record Timestamp", auto_now=True)
-    call_id = models.UUIDField("Call ID")
+    timestamp = models.DateTimeField("Record Timestamp", default=timezone.now)
+    call_id = models.IntegerField("Call ID")
     source = models.CharField(
         "Origin Phone Number", max_length=9, blank=False
     )
--- bills/tests.py
+++ bills/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from bills.models import CallRecord
+
+
+
+class TelephoneBillTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.last_month = timezone.now() - timedelta(days=30)
+        self.start_record1 = CallRecord.objects.create(
+            record_type="S", timestamp=timezone.now(), call_id=1,
+            source="12345678", destination="87654321")
+        self.start_record2 = CallRecord.objects.create(
+            record_type="S", timestamp=timezone.now(), call_id=2,
+            source="12345678", destination="87654321")
+        self.end_record1 = CallRecord.objects.create(
+            record_type="E", timestamp=timezone.now(), call_id=1,
+            source="12345678", destination="87654321")
+        self.end_record2 = CallRecord.objects.create(
+            record_type="E", timestamp=timezone.now(), call_id=2,
+            source="12345678", destination="87654321")
+        self.end_record3 = CallRecord.objects.create(
+            record_type="E", timestamp=timezone.now(), call_id=3,
+            source="12345678", destination="87654321")
+        # last month records
+        self.start_record4 = CallRecord.objects.create(
+            record_type="S", timestamp=self.last_month, call_id=4,
+            source="12345678", destination="87654321")
+        self.end_record4 = CallRecord.objects.create(
+            record_type="E", timestamp=self.last_month, call_id=4,
+            source="12345678", destination="87654321")
+
+
+    def test_get_bill_records(self):
+        expected_records = [
+            (self.start_record1, self.end_record1),
+            (self.start_record2, self.end_record2)
+        ]
+        bill_records = CallRecord.objects.get_bill_records("12345678")
+
+        self.assertEqual(expected_records, bill_records)
+
+    def test_get_bill_records_with_reference_period(self):
+        expected_records = [
+            (self.start_record4, self.end_record4)
+        ]
+        billing_cycle = f"{self.last_month.month:02}/{self.last_month.year}"
+        bill_records = CallRecord.objects.get_bill_records(
+            "12345678", billing_cycle)
+
+        self.assertEqual(expected_records, bill_records)
